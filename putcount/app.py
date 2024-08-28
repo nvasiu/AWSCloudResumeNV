0f96c84243dc83959952466918e0e843d2ecba94
nvasiu <nicholasvasiu@gmail.com>
2024-08-28 15:41:52 -0700

    Initial Commit

---
+++ putcount/app.py
@@ -0,0 +1,31 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+
+    table_name = "cloud-resume-nv"
+    table = dynamodb.Table(table_name)
+
+    try:
+        response = table.update_item(
+            Key={
+                'ID': '1'
+            },
+            UpdateExpression="ADD visitcount :val",
+            ExpressionAttributeValues={
+                ':val' : 1
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+    except Exception as e:
+        print(e)
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST"
+        }
+    }
